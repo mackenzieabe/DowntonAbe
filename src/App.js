a27138896f747078373fec2d6d22a00d4243f7c1
mackgabe <mackenzieabe@gmail.com>
2022-09-18 09:45:54 -0400

    created sendemail component and contact prop

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Nav from './components/Nav';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import About from './components/About';
-import ContactForm from './components/Contact';
+import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
+
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<About />} />
           <Route exact path='/portfolio' element={<Portfolio />} />
-          <Route exact path='/contact' element= {<ContactForm />}/>
+          <Route exact path='/contact' element= {<Contact/>}/>
           <Route exact path='/resume' element={<Resume />} />
         </Routes>
         {/* footer comp */}
---
+++ src/components/SendEmail/index.js
@@ -0,0 +1,54 @@
+import React, { useRef } from 'react';
+
+// import { useState, useEffect } from 'react'
+// import { validateEmail } from '../../utils/helpers';
+// import { useEffect } from 'react';
+import emailjs from "emailjs-com"
+
+
+export const Contact = (props) => {
+    const form = useRef();
+
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'template_7rytmg4', form.current, 'YnUNfDds0PBESJrg33')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            }
+            );
+        e.target.reset();
+
+
+        return (
+            <div>
+                <div className="container">
+                    <form onSubmit={sendEmail}>
+                        <div className="row pt-5 mx-auto">
+                            <div className="col-8 form-group mx-auto">
+                                <input type="text" className="form-control" placeholder="Name" name="name" />
+                            </div>
+                            <div className="col-8 form-group pt-2 mx-auto">
+                                <input type="text" className="form-control" placeholder="Email Address" name="email" />
+                            </div>
+                            <div className="col-8 form-group pt-2 mx-auto">
+                                <input type="text" className="form-control" placeholder="Subject" name="subject" />
+                            </div>
+                            <div className="col-8 form-group pt-2 mx-auto">
+                                <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+                            </div>
+                            <div className="col-8 pt-3 mx-auto">
+                                <input type="submit" className="btn btn-info" value="Send Message"></input>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    };
+
+}
+export default sendEmail;
