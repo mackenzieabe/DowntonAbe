c72195817830e81a3d4c684d914d775ab1f197d2
mackgabe <mackenzieabe@gmail.com>
2022-09-21 18:15:00 -0400

    tried several different css options

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,18 +6,25 @@ function About() {
     return (
 
         <section className="container-fluid my-5" >
-            {/* <h2 id="about" className="">About Me</h2> */}
+          
+            
             <div class="row">
-                <div class="col-lg-3">
-                    <img src='/images/mymug.JPG' className="my-2 rounded" style={{ width: "70%" }} alt="my face" />
-                </div>
-                <div id="about-me" className="col-lg-4 text-center font-weight-bold">
+                <div id="about-me" className="col text-center font-weight-bold">
                     <p>
-                        Hi! I'm Mackenzie, a web developer based out of Charlotte, NC. I love to learn, create and problem-solve. My interests include reading, gardening, animals and making things.</p>
+                        Hi! I'm Mackenzie, a web developer based out of Charlotte, NC. I love to learn, create and problem-solve.
+                        <br>
+                        </br>
+                        My interests include reading, gardening, animals and making things.</p>
                     <p>
+                        <br>
+                        </br>
                         You can reach me at mackenzieabe@gmail.com. Let's connect!
                     </p>
                 </div>
+                <div class="col-3">
+                    <img src='/images/mymug.JPG' id="myface" className="m-6 rounded" style={{ width: "80%" }} alt="my face" />
+                </div>
+
             </div>
         </section>
     )
--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,50 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './projectList.css'
 
-const ProjectList = ({ category }) => {
-
-  // const [currentPhoto, setCurrentPhoto] = useState();
-
-  const projects = [
-    {
-      name: "Word Raffle",
-      link: 'https://calm-plains-32572.herokuapp.com/',
-      src: '/images/wordraffle.png'
-    },
-    {
-      name: "The Quiz Wize",
-      link: 'https://mackenzieabe.github.io/TheQuizWiz/',
-      src: '/images/quizwiz screenshot1.png'
-    },
-    // {
-    //     name: <a href='https://mackenzieabe.github.io/TheQuizWiz/'>The Quiz Wiz</a>
-    // },
-    // {
-    //     name: <a href='https://mackenzieabe.github.io/DoorsofDurin/'>Doors of Durin</a>
-    // },
-    // {
-    //     name: <a href='https://konopie.github.io/cat-generator/'> Cat Genie </a>
-    // }
-  ]
-
-  // const currentPhotos = photos.filter(photo => photo.category === category);
+const ProjectList = ({ cateogry }) => {
 
   return (
-    <div>
-      <div className="flex-row">
-        {projects.map((image, i) => (
-          <div key={image.name}>
-            <h1>{image.name}</h1>
-            <a href={image.link}><img
-              src={image.src}
-              alt={image.name}
-              className="img-thumbnail mx-1"
-            /></a>
-          </div>
-        ))}
+    <div className="row">
+      <div className="col-md-6 m-4">
+        <div className="thumbnail">
+          <a href="https://calm-plains-32572.herokuapp.com/">
+            <img src="/images/wordraffle.png" alt="wordraffle screenshot" style={{ width: "65%" }}></img>
+
+          </a>
+        </div>
+      </div>
+      <div className="col-md-6 m-4">
+        <div className="thumbnail">
+          <a href="https://mackenzieabe.github.io/TheQuizWiz/">
+            <img src="/images/quizwiz.png" alt="quizqiz screenshot" style={{ width: "65%" }}></img>
+
+          </a>
+        </div>
+      </div>
+      <div className="col-md-6 m-4">
+        <div className="thumbnail">
+          <a href="https://konopie.github.io/cat-generator/">
+            <img src="/images/catgenie.png" alt="cat genie screenshot" style={{ width: "65%" }}></img>
+          </a>
+        </div>
+      </div>
+      <div className="col-md-6 m-4">
+        <div className="thumbnail">
+          <a href="https://mackenzieabe.github.io/DoorsofDurin/">
+            <img src="/images/doorsofdurin.png" alt="doors of durin screenshot" style={{ width: "65%" }}></img>
+          </a>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default ProjectList;
