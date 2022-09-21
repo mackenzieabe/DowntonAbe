5b84932fca5f34a650cc20e069303512b2918ab5
mackgabe <mackenzieabe@gmail.com>
2022-09-21 18:38:14 -0400

    fixed project list to have gallery of all of my work

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -4,39 +4,45 @@ import './projectList.css'
 const ProjectList = ({ cateogry }) => {
 
   return (
-    <div className="row">
-      <div className="col-md-6 m-4">
-        <div className="thumbnail">
-          <a href="https://calm-plains-32572.herokuapp.com/">
-            <img src="/images/wordraffle.png" alt="wordraffle screenshot" style={{ width: "65%" }}></img>
+    <section className="gallery min-vh-100">
+      <div className="container-lg">
+        <div className="row gy-4 row-cols-1 row-cols-sm-2 row-cols-md-3 ">
+          <div className="col">
+            <a href="https://calm-plains-32572.herokuapp.com/">
+              <img src="/images/wordraffle.png" alt="wordraffle screenshot"></img>
+            </a>
+          </div>
+          <div className="col">
+            <a href="https://mackenzieabe.github.io/TheQuizWiz/">
+              <img src="/images/quizwiz.png" alt="quizqiz screenshot"></img>
+            </a>
+          </div>
+          <div className="col">
+            <a href="https://konopie.github.io/cat-generator/">
+              <img src="/images/catgenie.png" alt="cat genie screenshot"></img>
+            </a>
+          </div>
+          <div className="col">
+            <a href="https://mackenzieabe.github.io/DoorsofDurin/">
+              <img src="/images/doorsofdurin.png" alt="doors of durin screenshot"></img>
+            </a>
+          </div>
+          <div className="col">
+            <a href="https://mackenzieabe.github.io/NotAlwaysSunny/">
+              <img src="/images/notalwayssunny.png" alt="weather dashboard screenshot"></img>
+            </a>
+          </div>
+          <div className="col">
+            <a href="https://noteablyawesome.herokuapp.com/notes">
+              <img src="/images/notetaker.png" alt="note taker screenshot"></img>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
 
-          </a>
-        </div>
-      </div>
-      <div className="col-md-6 m-4">
-        <div className="thumbnail">
-          <a href="https://mackenzieabe.github.io/TheQuizWiz/">
-            <img src="/images/quizwiz.png" alt="quizqiz screenshot" style={{ width: "65%" }}></img>
-
-          </a>
-        </div>
-      </div>
-      <div className="col-md-6 m-4">
-        <div className="thumbnail">
-          <a href="https://konopie.github.io/cat-generator/">
-            <img src="/images/catgenie.png" alt="cat genie screenshot" style={{ width: "65%" }}></img>
-          </a>
-        </div>
-      </div>
-      <div className="col-md-6 m-4">
-        <div className="thumbnail">
-          <a href="https://mackenzieabe.github.io/DoorsofDurin/">
-            <img src="/images/doorsofdurin.png" alt="doors of durin screenshot" style={{ width: "65%" }}></img>
-          </a>
-        </div>
-      </div>
-    </div>
   )
 }
+
 
 export default ProjectList;
