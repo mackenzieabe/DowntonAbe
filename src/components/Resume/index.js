e9ea950b1b5e54b5fc5a41426c5ddede2858073d
mackgabe <mackenzieabe@gmail.com>
2022-09-27 18:55:21 -0400

    tried adding pdf html with webpack

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
-import resume from '../../assets/myresume.pdf';
-import { Document, Page, pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import React from 'react';
+import resumeHtml from  '../../index.html'
+// import resume from '../../assets/myresume.pdf';
+// import { Document, Page, pdfjs } from 'react-pdf';
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
 function Resume() {
-  const [ numPages, setNumPages] = useState(null);
-  const [pageNumber] = useState(1);
+//   const [ numPages, setNumPages] = useState(null);
+//   const [pageNumber] = useState(1);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+//   function onDocumentLoadSuccess({ numPages }) {
+//     setNumPages(numPages);
+//   }
 
   return (
     <div>
-      <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-        </Document>
+    {resumeHtml}
       
     </div>
   );
