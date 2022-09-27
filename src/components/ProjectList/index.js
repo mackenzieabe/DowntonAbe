29dc15a44a2b74715fe18580237934dd2e61b1b0
mackgabe <mackenzieabe@gmail.com>
2022-09-27 09:01:58 -0400

    added email validation usestate hook and updated projectlist github link

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -11,31 +11,37 @@ const ProjectList = ({ cateogry }) => {
             <a href="https://calm-plains-32572.herokuapp.com/">
               <img src="/images/wordraffle.png" alt="wordraffle screenshot"></img>
             </a>
+            <a className='project-names' href='https://github.com/bethanyshoun/wordraffle.git'> Word Raffle </a>
           </div>
           <div className="col">
             <a href="https://mackenzieabe.github.io/TheQuizWiz/">
               <img src="/images/quizwiz.png" alt="quizqiz screenshot"></img>
             </a>
+            <a  className='project-names' href='https://github.com/mackenzieabe/TheQuizWiz.git'> The Quiz Wiz</a>
           </div>
           <div className="col">
             <a href="https://konopie.github.io/cat-generator/">
               <img src="/images/catgenie.png" alt="cat genie screenshot"></img>
             </a>
+            <a className='project-names' href='https://github.com/Konopie/cat-generator.git'> Cat Genie</a>
           </div>
           <div className="col">
             <a href="https://mackenzieabe.github.io/DoorsofDurin/">
               <img src="/images/doorsofdurin.png" alt="doors of durin screenshot"></img>
             </a>
+            <a className='project-names' href='https://github.com/mackenzieabe/DoorsofDurin.git'>Doors of Durin</a>
           </div>
           <div className="col">
             <a href="https://mackenzieabe.github.io/NotAlwaysSunny/">
               <img src="/images/notalwayssunny.png" alt="weather dashboard screenshot"></img>
             </a>
+            <a className='project-names' href='https://github.com/mackenzieabe/NotAlwaysSunny.git'>Weather Dashboard</a>
           </div>
           <div className="col">
             <a href="https://noteablyawesome.herokuapp.com/notes">
               <img src="/images/notetaker.png" alt="note taker screenshot"></img>
             </a>
+            <a className='project-names' href='https://github.com/mackenzieabe/notableyAwesome.git'>Note Taker</a>
           </div>
         </div>
       </div>
