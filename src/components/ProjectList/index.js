859ca26034865f42a10f1667be653f83f7add669
mackgabe <mackenzieabe@gmail.com>
2022-09-30 14:50:12 -0400

    moved images back to publlic folder

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -9,37 +9,37 @@ const ProjectList = ({ cateogry }) => {
         <div className="row gy-4 row-cols-1 row-cols-sm-2 row-cols-md-3 ">
           <div className="col">
             <a href="https://calm-plains-32572.herokuapp.com/">
-              <img src="/assets/images/wordraffle.png" alt="wordraffle screenshot"></img>
+              <img src="./images/wordraffle.png" alt="wordraffle screenshot"></img>
             </a>
             <a className='project-names' href='https://github.com/bethanyshoun/wordraffle.git'> Word Raffle </a>
           </div>
           <div className="col">
             <a href="https://mackenzieabe.github.io/TheQuizWiz/">
-              <img src="/assets/images/quizwiz.png" alt="quizqiz screenshot"></img>
+              <img src="./images/quizwiz.png" alt="quizqiz screenshot"></img>
             </a>
             <a  className='project-names' href='https://github.com/mackenzieabe/TheQuizWiz.git'> The Quiz Wiz</a>
           </div>
           <div className="col">
             <a href="https://konopie.github.io/cat-generator/">
-              <img src="/assets/images/catgenie.png" alt="cat genie screenshot"></img>
+              <img src="./images/catgenie.png" alt="cat genie screenshot"></img>
             </a>
             <a className='project-names' href='https://github.com/Konopie/cat-generator.git'> Cat Genie</a>
           </div>
           <div className="col">
             <a href="https://mackenzieabe.github.io/DoorsofDurin/">
-              <img src="/assets/images/doorsofdurin.png" alt="doors of durin screenshot"></img>
+              <img src="./images/doorsofdurin.png" alt="doors of durin screenshot"></img>
             </a>
             <a className='project-names' href='https://github.com/mackenzieabe/DoorsofDurin.git'>Doors of Durin</a>
           </div>
           <div className="col">
             <a href="https://mackenzieabe.github.io/NotAlwaysSunny/">
-              <img src="/assets/images/notalwayssunny.png" alt="weather dashboard screenshot"></img>
+              <img src="./images/notalwayssunny.png" alt="weather dashboard screenshot"></img>
             </a>
             <a className='project-names' href='https://github.com/mackenzieabe/NotAlwaysSunny.git'>Weather Dashboard</a>
           </div>
           <div className="col">
             <a href="https://noteablyawesome.herokuapp.com/notes">
-              <img src="/assets/images/notetaker.png" alt="note taker screenshot"></img>
+              <img src="./images/notetaker.png" alt="note taker screenshot"></img>
             </a>
             <a className='project-names' href='https://github.com/mackenzieabe/notableyAwesome.git'>Note Taker</a>
           </div>
