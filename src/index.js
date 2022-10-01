7aecb68e206054e6fb5a8513657d8e489f031b19
mackgabe <mackenzieabe@gmail.com>
2022-10-01 13:54:21 -0400

    back to browerrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 // import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
@@ -9,9 +9,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </React.StrictMode>
   );
 
