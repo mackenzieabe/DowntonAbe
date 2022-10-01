7b59db3c727fd08e63c25f66294a2c4007bad3c1
mackgabe <mackenzieabe@gmail.com>
2022-10-01 13:50:39 -0400

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-// import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'
+// import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>
+  );
 
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+
+// const container = document.getElementById('root');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
