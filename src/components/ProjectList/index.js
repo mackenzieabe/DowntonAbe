31240377019f73fa40ae923837f85ca9c6717594
mackgabe <mackenzieabe@gmail.com>
2022-10-01 18:01:46 -0400

    final push

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React from 'react'
 import './projectList.css'
-import Wordraffle from '../../assets/images/wordraffle.png'
+import wordraffle from '../../assets/images/wordraffle.png'
+import quizWiz from '../../assets/images/quizqiz.png'
+import catGenie from '../../assets/images/catgenie.png'
+import doorsOfDurin from '../../assets/images/doorsofdurin.png'
+import weatherDashboard from '../../assets/images/notalwayssunny.png'
+import noteTaker from '../../assets/images/notetaker.png'
 
 
 const ProjectList = ({ cateogry }) => {
@@ -11,37 +16,37 @@ const ProjectList = ({ cateogry }) => {
         <div className="row gy-4 row-cols-1 row-cols-sm-2 row-cols-md-3 ">
           <div className="col">
             <a href="https://calm-plains-32572.herokuapp.com/">
-              <img src={Wordraffle} alt="wordraffle screenshot"></img>
+              <img src={wordraffle} alt="wordraffle screenshot"></img>
             </a>
             <a className='project-names' href='https://github.com/bethanyshoun/wordraffle.git'> Word Raffle </a>
           </div>
           <div className="col">
             <a href="https://mackenzieabe.github.io/TheQuizWiz/">
-              <img src="../../assets/images/quizwiz.png" alt="quizqiz screenshot"></img>
+              <img src={quizWiz} alt="quizqiz screenshot"></img>
             </a>
             <a className='project-names' href='https://github.com/mackenzieabe/TheQuizWiz.git'> The Quiz Wiz</a>
           </div>
           <div className="col">
             <a href="https://konopie.github.io/cat-generator/">
-              <img src="../../assets/images/catgenie.png" alt="cat genie screenshot"></img>
+              <img src={catGenie} alt="cat genie screenshot"></img>
             </a>
             <a className='project-names' href='https://github.com/Konopie/cat-generator.git'> Cat Genie</a>
           </div>
           <div className="col">
             <a href="https://mackenzieabe.github.io/DoorsofDurin/">
-              <img src="../../assets/images/doorsofdurin.png" alt="doors of durin screenshot"></img>
+              <img src={doorsOfDurin} alt="doors of durin screenshot"></img>
             </a>
             <a className='project-names' href='https://github.com/mackenzieabe/DoorsofDurin.git'>Doors of Durin</a>
           </div>
           <div className="col">
             <a href="https://mackenzieabe.github.io/NotAlwaysSunny/">
-              <img src="../../assets/images/notalwayssunny.png" alt="weather dashboard screenshot"></img>
+              <img src={weatherDashboard} alt="weather dashboard screenshot"></img>
             </a>
             <a className='project-names' href='https://github.com/mackenzieabe/NotAlwaysSunny.git'>Weather Dashboard</a>
           </div>
           <div className="col">
             <a href="https://noteablyawesome.herokuapp.com/notes">
-              <img src="../../assets/images/notetaker.png" alt="note taker screenshot"></img>
+              <img src={noteTaker} alt="note taker screenshot"></img>
             </a>
             <a className='project-names' href='https://github.com/mackenzieabe/notableyAwesome.git'>Note Taker</a>
           </div>
